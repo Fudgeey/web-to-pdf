c4ff9371951ec8f676ee4f6e73829e5f68574f60
Luke Hallett <luke@larval.co.uk>
2022-09-22 22:03:22 +1200

    Fix CPU issue.

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/pdf', (req, res) => {
-    const browser = puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome',
-        args: ["--no-sandbox"]
-    });
-    
-    console.log(req.body.url);
-
     (async () => {
-        const page = await (await browser).newPage();
-        await page.goto(req.body.url);
-
-        const pdfStream = await page.createPDFStream({
-            format: 'a4',
-            margin: {
-                top: '0.4in',
-                right: '0.4in',
-                bottom: '0.4in',
-                left: '0.4in',
-            },
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome',
+            args: ["--no-sandbox"]
         });
 
-        res.attachment('booking.pdf');
-        pdfStream.pipe(res);
-        
-        pdfStream.on('end', async () => {
+        try {
+            const page = await (await browser).newPage();
+            await page.goto(req.body.url);
+
+            const pdf = await page.pdf({
+                format: 'a4',
+                margin: {
+                    top: '0.4in',
+                    right: '0.4in',
+                    bottom: '0.4in',
+                    left: '0.4in',
+                },
+            });
+
+            res.contentType('application/pdf');
+            res.send(pdf);
+        } catch(error) {
+            console.log(error);
+        } finally {
             await browser.close();
-        });
+        }
     })();
 });
 
